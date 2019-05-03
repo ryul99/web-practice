733ce065347012a1f43eabd0d2155d19ade7951d
Changmin Choi <cmchoi9901@gmail.com>
2019-05-03 17:34:48 +0900

    add model 'User', 'Post', 'Comment'

--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_time = models.DateTimeField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    content = models.TextField()
+    date_time = models.DateTimeField()
+    user = models.ForeignKey('User', on_delete = models.CASCADE)
+    post = models.ForeignKey('Post', on_delete = models.CASCADE)
