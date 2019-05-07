567ea517506f228a6d6253a65dec9c8c7850977b
Changmin Choi <cmchoi9901@gmail.com>
2019-05-07 10:06:39 +0900

    added django User

--- back/api/models.py
+++ back/api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=30)
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
