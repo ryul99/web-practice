21c1176565c0e613b43743636b9b31b5386f51ab
Changmin Choi <cmchoi9901@gmail.com>
2019-05-07 17:45:57 +0900

    added as_dict and related_name

--- back/api/models.py
+++ back/api/models.py
@@ -5,11 +5,29 @@ from django.utils.timezone import now
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_time = models.DateTimeField()
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    update_time = models.DateTimeField(default=now)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='post_set')
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content,
+            'update_time': self.update_time,
+            'user_id': self.user.id,
+            'comment_ids': list(self.comment_set.values_list('id', flat=True).all()),
+        }
 
 class Comment(models.Model):
     content = models.TextField()
-    date_time = models.DateTimeField()
-    user = models.ForeignKey('User', on_delete = models.CASCADE)
-    post = models.ForeignKey('Post', on_delete = models.CASCADE)
+    update_time = models.DateTimeField()
+    user = models.ForeignKey('User', on_delete = models.CASCADE, related_name='comment_set')
+    post = models.ForeignKey('Post', on_delete = models.CASCADE, related_name='comment_set')
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'update_time': self.update_time,
+            'user_id': self.user.id,
+        }
